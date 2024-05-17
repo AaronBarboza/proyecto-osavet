c9d0a571cf2aa2a163df2e7ea7b867e03fba7507
alemanGa08 <gabyaleman52@gmail.com>
2024-05-17 00:36:32 -0600

    cambios

--- lib/admin/pet_profile.dart
+++ lib/admin/pet_profile.dart
@@ -4,7 +4,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
-// Modelo para una mascota
 class Pet {
   final String name;
   final String type;
@@ -16,6 +15,7 @@ class Pet {
   final String allergies;
   final String currentMedications;
   final String vaccinations;
+  final String id; // Add an ID field
 
   Pet({
     required this.name,
@@ -28,6 +28,7 @@ class Pet {
     required this.allergies,
     required this.currentMedications,
     required this.vaccinations,
+    required this.id, // Add an ID field
   });
 
   Map<String, dynamic> toJson() {
@@ -43,6 +44,22 @@ class Pet {
       'currentMedications': currentMedications,
       'vaccinations': vaccinations,
     };
+  }
+
+  static Pet fromJson(Map<String, dynamic> json, String id) {
+    return Pet(
+      name: json['name'],
+      type: json['type'],
+      age: json['age'],
+      ownerName: json['ownerName'],
+      ownerPhone: json['ownerPhone'],
+      photoUrl: json['photoUrl'],
+      medicalConditions: json['medicalConditions'] ?? '',
+      allergies: json['allergies'] ?? '',
+      currentMedications: json['currentMedications'] ?? '',
+      vaccinations: json['vaccinations'] ?? '',
+      id: id,
+    );
   }
 }
 
@@ -81,18 +98,8 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
                 if (snapshot.hasData) {
                   final pets = snapshot.data!.docs.map((doc) {
                     final data = doc.data() as Map<String, dynamic>;
-                    return Pet(
-                      name: data['name'],
-                      type: data['type'],
-                      age: data['age'],
-                      ownerName: data['ownerName'],
-                      ownerPhone: data['ownerPhone'],
-                      photoUrl: data['photoUrl'],
-                      medicalConditions: data['medicalConditions'] ?? '',
-                      allergies: data['allergies'] ?? '',
-                      currentMedications: data['currentMedications'] ?? '',
-                      vaccinations: data['vaccinations'] ?? '',
-                    );
+                    final id = doc.id;
+                    return Pet.fromJson(data, id);
                   }).toList();
 
                   return ListView.builder(
@@ -116,6 +123,9 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
                             Text('${pet.ownerPhone}'),
                           ],
                         ),
+                        onTap: () {
+                          _showPetDetailsDialog(context, pet);
+                        },
                       );
                     },
                   );
@@ -133,6 +143,56 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> _showPetDetailsDialog(BuildContext context, Pet pet) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(pet.name),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                pet.photoUrl.isNotEmpty
+                    ? Image.network(pet.photoUrl)
+                    : const Icon(Icons.pets, size: 100),
+                Text('Raza: ${pet.type}'),
+                Text('Edad: ${pet.age} años'),
+                Text('Nombre del Dueño: ${pet.ownerName}'),
+                Text('Teléfono del Dueño: ${pet.ownerPhone}'),
+                Text('Condiciones Médicas: ${pet.medicalConditions}'),
+                Text('Alergias: ${pet.allergies}'),
+                Text('Medicamentos Actuales: ${pet.currentMedications}'),
+                Text('Vacunas: ${pet.vaccinations}'),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                _showEditPetDialog(context, pet);
+              },
+              child: const Text('Editar'),
+            ),
+            TextButton(
+              onPressed: () {
+                _deletePet(pet.id);
+                Navigator.pop(context);
+              },
+              child: const Text('Eliminar'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cerrar'),
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -248,6 +308,134 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
     );
   }
 
+  Future<void> _showEditPetDialog(BuildContext context, Pet pet) async {
+    // Fill the controllers with the existing data
+    _nameController.text = pet.name;
+    _typeController.text = pet.type;
+    _ageController.text = pet.age.toString();
+    _ownerNameController.text = pet.ownerName;
+    _ownerPhoneController.text = pet.ownerPhone;
+    _medicalConditionsController.text = pet.medicalConditions;
+    _allergiesController.text = pet.allergies;
+    _currentMedicationsController.text = pet.currentMedications;
+    _vaccinationsController.text = pet.vaccinations;
+
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Editar Mascota'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () async {
+                    final pickedImage = await _imagePicker.pickImage(source: ImageSource.gallery);
+                    if (pickedImage != null) {
+                      setState(() {
+                        _selectedImage = File(pickedImage.path);
+                      });
+                    }
+                  },
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[300],
+                    ),
+                    child: _selectedImage != null
+                        ? Image.file(
+                            _selectedImage!,
+                            fit: BoxFit.cover,
+                          )
+                        : pet.photoUrl.isNotEmpty
+                            ? Image.network(
+                                pet.photoUrl,
+                                fit: BoxFit.cover,
+                              )
+                            : const Icon(Icons.camera_alt),
+                  ),
+                ),
+                TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre',
+                  ),
+                ),
+                TextField(
+                  controller: _typeController,
+                  decoration: const InputDecoration(
+                    labelText: 'Raza',
+                  ),
+                ),
+                TextField(
+                  controller: _ageController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Edad',
+                  ),
+                ),
+                TextField(
+                  controller: _ownerNameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nombre del Dueño',
+                  ),
+                ),
+                TextField(
+                  controller: _ownerPhoneController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    labelText: 'Teléfono del Dueño',
+                  ),
+                ),
+                TextField(
+                  controller: _medicalConditionsController,
+                  decoration: const InputDecoration(
+                    labelText: 'Condiciones Médicas',
+                  ),
+                ),
+                TextField(
+                  controller: _allergiesController,
+                  decoration: const InputDecoration(
+                    labelText: 'Alergias',
+                  ),
+                ),
+                TextField(
+                  controller: _currentMedicationsController,
+                  decoration: const InputDecoration(
+                    labelText: 'Medicamentos Actuales',
+                  ),
+                ),
+                TextField(
+                  controller: _vaccinationsController,
+                  decoration: const InputDecoration(
+                    labelText: 'Vacunas',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                _updatePet(context, pet.id);
+                Navigator.pop(context);
+              },
+              child: const Text('Guardar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   Future<void> _addPet(BuildContext context) async {
     String? photoUrl;
     if (_selectedImage != null) {
@@ -268,6 +456,7 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
       allergies: _allergiesController.text,
       currentMedications: _currentMedicationsController.text,
       vaccinations: _vaccinationsController.text,
+      id: '', // Empty ID for new pets
     );
 
     try {
@@ -280,6 +469,58 @@ class _PetProfileScreenState extends State<PetProfileScreen> {
       );
     }
 
+    _clearControllers();
+  }
+
+  Future<void> _updatePet(BuildContext context, String id) async {
+    String? photoUrl;
+    if (_selectedImage != null) {
+      final storageRef = FirebaseStorage.instance.ref().child('pet_photos').child('${DateTime.now().millisecondsSinceEpoch}.jpg');
+      final uploadTask = storageRef.putFile(_selectedImage!);
+      await uploadTask.whenComplete(() => null);
+      photoUrl = await storageRef.getDownloadURL();
+    }
+
+    final pet = Pet(
+      name: _nameController.text,
+      type: _typeController.text,
+      age: int.parse(_ageController.text),
+      ownerName: _ownerNameController.text,
+      ownerPhone: _ownerPhoneController.text,
+      photoUrl: photoUrl ?? '',
+      medicalConditions: _medicalConditionsController.text,
+      allergies: _allergiesController.text,
+      currentMedications: _currentMedicationsController.text,
+      vaccinations: _vaccinationsController.text,
+      id: id,
+    );
+
+    try {
+      await FirebaseFirestore.instance.collection('pets').doc(id).update(pet.toJson());
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error al actualizar la mascota: $e'),
+        ),
+      );
+    }
+
+    _clearControllers();
+  }
+
+  Future<void> _deletePet(String id) async {
+    try {
+      await FirebaseFirestore.instance.collection('pets').doc(id).delete();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error al eliminar la mascota: $e'),
+        ),
+      );
+    }
+  }
+
+  void _clearControllers() {
     _nameController.clear();
     _typeController.clear();
     _ageController.clear();
